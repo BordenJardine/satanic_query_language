ae263a83fbb4b9f3ec1d9706a97f320ff5d72964
Matt J <bordenjardine@gmail.com>
2018-07-19 20:47:53 -0400

    csv

---
+++ app/loader.rb
@@ -0,0 +1,22 @@
+require 'active_record'
+require 'mysql2'
+require 'pry'
+require 'csv'
+
+# Load Up The Satanic Glossary Database
+#
+# Load access_notes
+# load citations
+# load instances
+# load term_instances
+# load terms
+#
+#
+#
+# for each row
+# create term
+#   - if citation_url found use that one
+#     - otherwise create one
+#   - if citation_url found use that one
+#     - otherwise create one
+
